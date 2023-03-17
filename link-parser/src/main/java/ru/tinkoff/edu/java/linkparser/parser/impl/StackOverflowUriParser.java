4e50a97efd92444f4d9a748627f944a3bf1398e7
VladimirZaitsev21 <vlzaitsev2000@gmail.com>
2023-03-17 22:48:52 +0300

    Renaming some classes with *url* to *uri*.

--- link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/parser/impl/StackOverflowUriParser.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/parser/impl/StackOverflowUriParser.java
@@ -1,25 +1,25 @@
 package ru.tinkoff.edu.java.linkparser.parser.impl;
 
-import ru.tinkoff.edu.java.linkparser.model.answer.StackOverflowUrlParserAnswer;
-import ru.tinkoff.edu.java.linkparser.model.answer.UrlParserAnswer;
-import ru.tinkoff.edu.java.linkparser.parser.api.CommonUrlParser;
+import ru.tinkoff.edu.java.linkparser.model.answer.StackOverflowUriParserAnswer;
+import ru.tinkoff.edu.java.linkparser.model.answer.UriParserAnswer;
+import ru.tinkoff.edu.java.linkparser.parser.api.CommonUriParser;
 
 import java.net.URI;
 import java.util.regex.Pattern;
 
-public final class StackOverflowUrlParser extends CommonUrlParser {
+public final class StackOverflowUriParser extends CommonUriParser {
 
     public static final Pattern PATTERN = Pattern.compile("/(questions)/(\\d{1,20})");
     public static final int QUESTION_ID_REGEXP_GROUP_NUMBER = 2;
 
-    public StackOverflowUrlParser(String processedAuthority) {
+    public StackOverflowUriParser(String processedAuthority) {
         super(processedAuthority, PATTERN);
     }
 
     @Override
-    protected UrlParserAnswer extractPayloadFromUrl(URI parsedUri) {
+    protected UriParserAnswer extractPayloadFromUrl(URI parsedUri) {
         var matcher = pattern.matcher(parsedUri.getPath());
-        if (matcher.find()) return new StackOverflowUrlParserAnswer(
+        if (matcher.find()) return new StackOverflowUriParserAnswer(
                 Long.parseLong(matcher.group(QUESTION_ID_REGEXP_GROUP_NUMBER))
         );
         else return null;
--- link-parser/src/test/java/ru/tinkoff/edu/java/linkparser/parser/UriParsersChainTest.java
+++ link-parser/src/test/java/ru/tinkoff/edu/java/linkparser/parser/UriParsersChainTest.java
@@ -4,29 +4,29 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import ru.tinkoff.edu.java.linkparser.model.UserAndRepo;
-import ru.tinkoff.edu.java.linkparser.model.answer.GitHubUrlParserAnswer;
-import ru.tinkoff.edu.java.linkparser.model.answer.StackOverflowUrlParserAnswer;
-import ru.tinkoff.edu.java.linkparser.model.answer.UrlParserAnswer;
-import ru.tinkoff.edu.java.linkparser.parser.api.UrlParser;
-import ru.tinkoff.edu.java.linkparser.parser.impl.GitHubUrlParser;
-import ru.tinkoff.edu.java.linkparser.parser.impl.StackOverflowUrlParser;
-import ru.tinkoff.edu.java.linkparser.parser.impl.UnsupportedUrlParser;
+import ru.tinkoff.edu.java.linkparser.model.answer.GitHubUriParserAnswer;
+import ru.tinkoff.edu.java.linkparser.model.answer.StackOverflowUriParserAnswer;
+import ru.tinkoff.edu.java.linkparser.model.answer.UriParserAnswer;
+import ru.tinkoff.edu.java.linkparser.parser.api.UriParser;
+import ru.tinkoff.edu.java.linkparser.parser.impl.GitHubUriParser;
+import ru.tinkoff.edu.java.linkparser.parser.impl.StackOverflowUriParser;
+import ru.tinkoff.edu.java.linkparser.parser.impl.UnsupportedUriParser;
 
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class UrlParsersChainTest {
+public class UriParsersChainTest {
 
-    private final UrlParser gitHubUrlParser = new GitHubUrlParser("github.com");
-    private final UrlParser stackOverflowUrlParser = new StackOverflowUrlParser("stackoverflow.com");
-    private final UrlParser unsupportedUrlParser = new UnsupportedUrlParser();
+    private final UriParser gitHubUriParser = new GitHubUriParser("github.com");
+    private final UriParser stackOverflowUriParser = new StackOverflowUriParser("stackoverflow.com");
+    private final UriParser unsupportedUriParser = new UnsupportedUriParser();
 
-    private final UrlParsersChain parsersChain = new UrlParsersChain(gitHubUrlParser, stackOverflowUrlParser, unsupportedUrlParser);
+    private final UrlParsersChain parsersChain = new UrlParsersChain(gitHubUriParser, stackOverflowUriParser, unsupportedUriParser);
 
     @ParameterizedTest
     @MethodSource("getArgumentsForDoParseTest")
-    public void testDoParse(String source, UrlParserAnswer expected) {
+    public void testDoParse(String source, UriParserAnswer expected) {
         var actual = parsersChain.doParse(source);
         assertEquals(expected, actual);
     }
@@ -35,11 +35,11 @@ public class UrlParsersChainTest {
         return Stream.of(
                 Arguments.of(
                     "https://github.com/VladimirZaitsev21/some-repo",
-                    new GitHubUrlParserAnswer(new UserAndRepo("VladimirZaitsev21", "some-repo"))
+                    new GitHubUriParserAnswer(new UserAndRepo("VladimirZaitsev21", "some-repo"))
                 ),
                 Arguments.of(
                     "https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c",
-                    new StackOverflowUrlParserAnswer(1642028)
+                    new StackOverflowUriParserAnswer(1642028)
                 ),
                 Arguments.of(
                     "https://github.com/VladimirZaitsev21?tab=repositories",
